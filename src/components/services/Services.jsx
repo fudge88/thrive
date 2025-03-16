4690ef6c41706f21e0ce3250a66682688a237f5d
fahra akhlaq <f.akhlaq@outlook.com>
2025-03-16 12:17:07 +0000

    buttons for services added

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import './services.css';
-
 const Services = () => {
-	return <div className="services">services</div>;
+	return (
+		<div className="servicesContainer">
+			<div className="servicesBtn fitness">
+				<h4>Wellness & Fitness</h4>
+				<h2>Personal Training</h2>
+			</div>
+
+			<div className="servicesBtn rehab">
+				<h4>Diagnostics & Treatment</h4>
+				<h2>Rehabilitation & Recovery</h2>
+			</div>
+
+			<div className="servicesBtn nutrition">
+				<h4>Optimal Health</h4>
+				<h2>Diet & Nutrition</h2>
+			</div>
+		</div>
+	);
 };
 
 export default Services;
